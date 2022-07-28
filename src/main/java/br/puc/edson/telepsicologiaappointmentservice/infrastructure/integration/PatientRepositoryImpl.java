e63cb09c54a367d725a13aa460bd9f959f96df80
Edson Galdino da Silva Alves <edson1galdino@gmail.com>
2022-07-28 19:23:06 -0300

    Adicionando integração com outros microserviços e deploy na aws

--- src/main/java/br/puc/edson/telepsicologiaappointmentservice/infrastructure/integration/PatientRepositoryImpl.java
+++ src/main/java/br/puc/edson/telepsicologiaappointmentservice/infrastructure/integration/PatientRepositoryImpl.java
@@ -2,12 +2,24 @@ package br.puc.edson.telepsicologiaappointmentservice.infrastructure.integration
 
 import br.puc.edson.telepsicologiaappointmentservice.domain.model.Appointment;
 import br.puc.edson.telepsicologiaappointmentservice.infrastructure.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PatientRepositoryImpl implements PatientRepository {
+
+    private final RestTemplate restTemplate;
+
+    private final String patientApiUrl;
+
+    public PatientRepositoryImpl(RestTemplate restTemplate, @Value("${api.patient.url}")  String patientApiUrl) {
+        this.restTemplate = restTemplate;
+        this.patientApiUrl = patientApiUrl;
+    }
+
     @Override
     public Appointment.Patient findPatientById(String id) {
-        return Appointment.Patient.builder().id(id).build();
+        return restTemplate.getForEntity(patientApiUrl + "/patient/" + id, Appointment.Patient.class).getBody();
     }
 }
--- src/main/java/br/puc/edson/telepsicologiaappointmentservice/infrastructure/integration/PsychologistRepositoryImpl.java
+++ src/main/java/br/puc/edson/telepsicologiaappointmentservice/infrastructure/integration/PsychologistRepositoryImpl.java
@@ -2,12 +2,25 @@ package br.puc.edson.telepsicologiaappointmentservice.infrastructure.integration
 
 import br.puc.edson.telepsicologiaappointmentservice.domain.model.Appointment;
 import br.puc.edson.telepsicologiaappointmentservice.infrastructure.repository.PsychologistRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PsychologistRepositoryImpl implements PsychologistRepository {
+
+    private final RestTemplate restTemplate;
+
+    private final String psychologistApiUrl;
+
+    public PsychologistRepositoryImpl(RestTemplate restTemplate, @Value("${api.psychologist.url}")  String psychologistApiUrl) {
+        this.restTemplate = restTemplate;
+        this.psychologistApiUrl = psychologistApiUrl;
+    }
+
     @Override
     public Appointment.Psychologist findPsychologistById(String id) {
-        return Appointment.Psychologist.builder().id(id).build();
+        return restTemplate.getForEntity(psychologistApiUrl + "/psychologist/" + id, Appointment.Psychologist.class).getBody();
     }
+
 }
