a39348092a18978b12e51ea322187d40c0d32c8d
Edson Galdino da Silva Alves <edson1galdino@gmail.com>
2022-08-02 21:48:18 -0300

    Ajustando dtos de posts

--- src/main/java/br/puc/edson/telepsicologiaappointmentservice/entrypoint/dto/NewAppointmentDto.java
+++ src/main/java/br/puc/edson/telepsicologiaappointmentservice/entrypoint/dto/NewAppointmentDto.java
@@ -1,13 +1,17 @@
 package br.puc.edson.telepsicologiaappointmentservice.entrypoint.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewAppointmentDto {
 
     @NotNull
--- src/main/java/br/puc/edson/telepsicologiaappointmentservice/entrypoint/dto/ReplyRequestDto.java
+++ src/main/java/br/puc/edson/telepsicologiaappointmentservice/entrypoint/dto/ReplyRequestDto.java
@@ -1,12 +1,16 @@
 package br.puc.edson.telepsicologiaappointmentservice.entrypoint.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReplyRequestDto {
 
     @NotNull
